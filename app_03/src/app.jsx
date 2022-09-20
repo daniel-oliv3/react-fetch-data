6c731e1c6c7ebaf3da76e92037c8eae7e6179c78
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-20 09:19:57 -0300

    Update app.jsx

--- app_03/src/app.jsx
+++ app_03/src/app.jsx
@@ -1,6 +1,17 @@
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function App(){
+
+    const [produtos, setProdutos] = useState({});
+
+    useEffect(() => {
+        axios.get('https://dummyjson.com/products')
+        .then((response) => {
+            console.log(response)
+        })
+    }, []);
+
     return (
         <>
             <h1>React Axios</h1>
